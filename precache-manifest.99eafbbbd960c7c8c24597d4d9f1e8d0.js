25a09a3250393acc25808debeaa31ad350b8a904
c-h-o-c-o-l-a-t-e <what.the.hell.github@gmail.com>
2020-09-17 01:37:26 +0530

    Updates

---
+++ precache-manifest.99eafbbbd960c7c8c24597d4d9f1e8d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b4700c9d95fbab38cfb0083d14c090",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "d7464b929a4ae81ea30a",
+    "url": "/pomodoro-clock/static/css/main.70058789.chunk.css"
+  },
+  {
+    "revision": "ccb5e2dfce02c4609003",
+    "url": "/pomodoro-clock/static/js/2.2f74119b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pomodoro-clock/static/js/2.2f74119b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7464b929a4ae81ea30a",
+    "url": "/pomodoro-clock/static/js/main.053c5d79.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
